6555d29170f66441c097e8665ff4683992a50c56
Chere3 <71246795+Chere3@users.noreply.github.com>
2024-10-09 22:52:12 -0600

    feat(events): add client.uploadCommands() to ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,5 +4,6 @@ export default createEvent({
     data: { once: true, name: "botReady" },
     run(user, client) {
         client.logger.info(`Bot ready as ${user.tag}`);
+        client.uploadCommands();
     }
 })
