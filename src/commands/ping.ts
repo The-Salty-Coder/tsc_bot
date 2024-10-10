dd5b1e2109d7ee299107bcf588181310bc429474
Chere3 <71246795+Chere3@users.noreply.github.com>
2024-10-09 22:53:42 -0600

    feat(commands): update ping command to display bot and message latency

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -8,14 +8,14 @@ export default class Ping extends Command {
     async run(ctx: CommandContext) {
         const date = Date.now();
         const embed = new Embed({
-            description: `🏓 **${ctx.client.gateway.latency}**`
+            description: `🏓 Latencia del bot: **${ctx.client.gateway.latency}ms**`
         })
 
         await ctx.editOrReply({ embeds: [embed] });
         const message = await ctx.fetchResponse();
         const ping = message?.createdTimestamp! - date;
 
-        embed.setDescription(`🏓 **${ctx.client.gateway.latency}**\n🕒 **${ping}ms**`);
+        embed.setDescription(`🏓 Latencia del bot: **${ctx.client.gateway.latency}ms**\n🕒 Latencia de los mensajes: **${ping}ms**`);
         await ctx.editOrReply({ embeds: [embed] });
     }
 }
