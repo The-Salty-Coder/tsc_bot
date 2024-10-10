6dd647cae9909653532729f9ab3772329fe95ff8
Chere3 <71246795+Chere3@users.noreply.github.com>
2024-10-09 22:50:31 -0600

    feat(events): add ready event to log bot readiness

---
+++ src/events/ready.ts
@@ -0,0 +1,8 @@
+import { createEvent } from "seyfert";
+
+export default createEvent({
+    data: { once: true, name: "botReady" },
+    run(user, client) {
+        client.logger.info(`Bot ready as ${user.tag}`);
+    }
+})
